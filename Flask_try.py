deea82f8f27209f6e443fb1ce41124b0d3a87ac3
thesrsakabuvttchi <58292377+thesrsakabuvttchi@users.noreply.github.com>
2021-03-31 17:24:17 +0530

    Update Flask_try.py

--- Flask_try.py
+++ Flask_try.py
@@ -88,7 +88,7 @@ def GetEvents():
         summary = i['summary']
         begin = i['start'][(next(iter(i['start'])))]
         end = i['end'][next(iter(i['end']))]
-        Events.append({'Title':summary,'Begin':begin,'End':end})
+        Events.append({'Title':summary,'Begin':begin[11:15],'End':end})
 
     JSON = {'events':Events}
     JSON = json.dumps(JSON, indent = 4)  
